c61199dcf83c62753cc75c194c642e279856ce5f
Sonu Kumar <skbhati199@gmail.com>
2020-07-03 22:49:58 +0530

    Initial commit

---
+++ bluefluper/src/main/java/com/fluper/bluefluper/BlueActivity.kt
@@ -0,0 +1,12 @@
+package com.fluper.bluefluper
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class BlueActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_blue)
+    }
+}
