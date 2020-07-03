121b1ab0b1ab49da2bfa6e00fbdce87d3a384edf
Sonu Kumar <skbhati199@gmail.com>
2020-07-03 23:33:37 +0530

    release 1.0.1

--- app/src/main/java/com/fluper/blueflupter/MainActivity.kt
+++ app/src/main/java/com/fluper/blueflupter/MainActivity.kt
@@ -2,11 +2,9 @@ package com.fluper.blueflupter
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.fluper.bluefluper.BlueActivity
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BlueActivity() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-    }
+
 }
--- bluefluper/src/main/java/com/fluper/bluefluper/BlueActivity.kt
+++ bluefluper/src/main/java/com/fluper/bluefluper/BlueActivity.kt
@@ -3,7 +3,7 @@ package com.fluper.bluefluper
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class BlueActivity : AppCompatActivity() {
+abstract class BlueActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
